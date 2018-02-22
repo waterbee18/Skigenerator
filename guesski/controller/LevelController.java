c49626e208c2cb7cc04010779255f5e42b3740c3
waterbee18 <gabrou09@hotmail.com>
2018-02-22 09:59:19 -0500

    Level update (reverted from commit 43778a8bff998b7a9dfed3fe8f82fa13ea284a01)

--- guesski/controller/LevelController.java
+++ guesski/controller/LevelController.java
@@ -1,62 +1,50 @@
 package guesski.controller;
 
-import guesski.model.Grille;
-import guesski.model.LevelInfo;
-import guesski.model.Ramp;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
 
 public class LevelController
 {
 
-    @FXML
-    Rectangle cible;
-    @FXML
-    AnchorPane anchor;
-    LevelInfo levelinfo;
-    Grille grille;
-
-    public LevelController(){
-        levelinfo = new LevelInfo();
-        grille = new Grille();
-    }
-
-    @FXML
-    public void initialize() {
-        Ramp ramp = levelinfo.getTg().getRamp();
-        ramp.setTranslateY(100);
-        anchor.getChildren().add(ramp);
-        anchor.getChildren().add(grille);
-    }
-
     public void closeGame(){
         GameMaster.closeGame();
     }
+
+
+    @FXML
+    Rectangle cible;
+
     public void dragDetected()
     {
-        Dragboard dragboard = cible.startDragAndDrop(TransferMode.MOVE);
+        /*Dragboard dragboard = cible.startDragAndDrop(TransferMode.MOVE);
         ClipboardContent contenu = new ClipboardContent();
         contenu.putString("Dropped");
         dragboard.setContent(contenu);
         return;
     }
 
-    public void dragOver(DragEvent event)
-    {
-        event.acceptTransferModes(TransferMode.MOVE);
-        event.consume();
-    }
 
-    public void dragDropped (DragEvent event)
+    public LevelController()
     {
-        String content = event.getDragboard().getString();
-        System.out.println(content);
+//        cible.setOnDragDetected(event-> {
 
-        event.setDropCompleted(true);
+        });
+
+        cible.setOnDragOver(event -> {
+            event.acceptTransferModes(TransferMode.MOVE);
+            event.consume();
+        });
+
+        cible.setOnDragDropped(event -> {
+            String content = event.getDragboard().getString();
+            System.out.println(content);
+
+            event.setDropCompleted(true);
+            });*/
     }
 }
